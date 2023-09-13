04d1c57ae431283b98f134d5a4e9f58c69c4d2c3
hiftielabs <hiftielabs@gmail.com>
2023-09-13 13:01:41 +0530

    🔨 Fixed Animations

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,12 +3,16 @@ import ChannelWelcome from '../assets/svgs/chnl-wlcm.svg';
 import GetStarted from '../assets/svgs/get-started-button.svg';
 import SupportServer from '../assets/svgs/support-server-button.svg';
 
+function SupportServerLink() {
+    var url = 'https://discord.gg/voiceplus';
+    window.open(url, '_blank');
+  }
 
 function Hero() {
     return (
         <main className="hero-section">
             <div className='hero-content'>
-                <h1 className='hero-title'>Voice+ is your all-in-one voice channel manager</h1>
+                <h1 className='hero-title'><span onClick={SupportServerLink} className='voice-plus-text'>Voice+</span> is your all-in-one voice channel manager</h1>
                 <p className='hero-subtitle'>Managing your temporary voice channels have never been easier.</p>
                 <img className='get-started-button' src={GetStarted} alt='get-started-button' draggable='false'></img>
                 <img src={SupportServer} className='support-server-button' alt='support-server-button' draggable='false'></img>
